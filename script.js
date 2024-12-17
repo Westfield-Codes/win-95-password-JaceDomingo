7aab9ac6fdd995bb45e8dfbdf60e3f60d2209502
JaceDomingo <jace208753@student-schoolsofwestfield.org>
2024-12-17 15:04:30 +0000

    working

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function setup(){
+    let buttons = document.getElementById("buttons");
+    let OK = document.createElement("button");
+    OK.innerHTML = "OK";
+    buttons.appendChild(OK);
+    let cancel = document.createElement("button");
+    cancel.innerHTML = 
+}
