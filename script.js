1d304dd6144be16a332880524dd1a1b60d146d09
JaceDomingo <jace208753@student-schoolsofwestfield.org>
2024-12-18 14:52:37 +0000

    working

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ function setup(){
     let buttons = document.getElementById("buttons");
     let OK = document.createElement("button");
     OK.innerHTML = "OK";
+    OK.addEventListener("click",getPassword);
     buttons.appendChild(OK);
     let cancel = document.createElement("button");
-    cancel.innerHTML = 
+    cancel.innerHTML = "cancel";
+    buttons.appendChild(cancel);
+}
+function getPassword(){
+    let dialogs = document.getElementById("dialogs");
+    let username = dialogs.querySelector(':nth-child(2)').value;
+    let password = dialogs.querySelector(':nth-child(4)').value;
+    alert(username+password);
 }
